8386e4426ff59aeece0a418a49384baeb2f9a9d4
Anas Sibarni <anas.sibarni.etu@univ-lille.fr>
2025-08-15 20:43:08 +0200

    onglet groups lié

---
+++ scripts/tabs.js
@@ -0,0 +1,18 @@
+if (!window.userId) {
+    window.userId = localStorage.getItem('userId');
+  }
+  
+
+// Quand on clique sur  Discussions
+document.getElementById('nav-discussions').addEventListener('click', () => {
+    document.getElementById('contact-section').style.display = 'block'; // discussions
+    document.getElementById('group-section').style.display = 'none';    // groupes
+  });
+
+  // Quand on clique sur  Groupes
+  document.getElementById('nav-groups').addEventListener('click', () => {
+    document.getElementById('contact-section').style.display = 'none';  // discussions
+    document.getElementById('group-section').style.display = 'block';   // groupes
+    loadUserGroups(); // appel direct à ta fonction groups.js
+  });
+
